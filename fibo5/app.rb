6708c61ef7832f74a3d30e4c824450e0e0437d5c
rodrigo <r.pasquali2412@gmail.com>
2018-05-29 01:42:04 -0300

    Se agrega a app.rb ruta para obtener sentido inverso de una lista

--- fibo5/app.rb
+++ fibo5/app.rb
@@ -2,11 +2,26 @@ require 'sinatra'
 require 'sinatra/json'
 require_relative './model/calculador_fibonacci'
 require_relative './model/calculador_fibonacci_lista'
+require_relative './model/sentido'
 
 get '/fibonacci/:n/:funcionamiento' do
   numero = params[:n].to_i
   funcionamiento = params[:funcionamiento]
   calculador_fibo = CalculadorFibonacciLista.new
   resultado = calculador_fibo.calcular_fibo(numero, funcionamiento)
+  json({"fibonacci": { "limite": numero, "lista": resultado } })
+end
+
+get '/fibonacci/:n' do
+  numero = params[:n].to_i
+  sentido = params[:sentido].to_s
+  funcionamiento = 'lista'
+  calculador_fibo = CalculadorFibonacciLista.new
+  aplicador_sentido = Sentido.new
+  lista_numeros = calculador_fibo.calcular_fibo(numero, funcionamiento)
+  resultado = 0
+  if(sentido == 'inverso')
+  	resultado = aplicador_sentido.sentido_inverso(lista_numeros)
+  end
   json({"fibonacci": { "limite": numero, "lista": resultado } })
 end
--- fibo5/model/sentido.rb
+++ fibo5/model/sentido.rb
@@ -1,5 +1,5 @@
 class Sentido
 	def sentido_inverso(lista_numeros)
-		return lista_numeros.sort.reverse
+		return lista_numeros.sort.reverse!
 	end
 end
