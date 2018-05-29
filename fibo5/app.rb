1f123dfa6e9216c5df727dd1f66077e6cd190b77
rodrigo <r.pasquali2412@gmail.com>
2018-05-29 00:10:54 -0300

    Se agrega en app metodo para realizar sumatoria y se prueba get fibo/sumatoria

--- fibo5/app.rb
+++ fibo5/app.rb
@@ -2,9 +2,16 @@ require 'sinatra'
 require 'sinatra/json'
 require_relative './model/calculador_fibonacci'
 
-get '/fibonacci/:n' do
+get '/fibonacci/:n/lista' do
   numero = params[:n].to_i
   calculador_fibo = CalculadorFibonacci.new
   resultado = calculador_fibo.calcular_en_lista(numero)
+  json({"fibonacci": { "limite": numero, "lista": resultado } })
+end
+
+get '/fibonacci/:n/sumatoria' do
+  numero = params[:n].to_i
+  calculador_fibo = CalculadorFibonacci.new
+  resultado = calculador_fibo.calcular_sumatoria(numero)
   json({"fibonacci": { "limite": numero, "lista": resultado } })
 end
